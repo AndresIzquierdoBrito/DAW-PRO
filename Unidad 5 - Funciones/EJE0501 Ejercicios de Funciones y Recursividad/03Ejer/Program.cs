d068bd88da2e4e356df2ab59c794167bb29dede4
Andres Izquierdo <andres.izbri@gmail.com>
2023-01-17 12:16:47 +0000

    Added Function Exercise

--- Unidad 5 - Funciones/EJE0501 Ejercicios de Funciones y Recursividad/03Ejer/Program.cs
+++ Unidad 5 - Funciones/EJE0501 Ejercicios de Funciones y Recursividad/03Ejer/Program.cs
@@ -1,7 +1,29 @@
-﻿internal class Program
+﻿namespace Ejer03
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello, World!");
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un valor entero, solo entero, intenta poner otra cosa, atrevete.");
+            GetValidated.IntValue();
+            Console.WriteLine("Te dije, solo entero jeje");
+        }
     }
+    class GetValidated
+    {
+        public static int IntValue()
+        {
+            bool check = false;
+            int returnedInt;
+            do
+            {
+                if (int.TryParse(Console.ReadLine(), out returnedInt))
+                    check = true;
+                else Console.WriteLine("Valor no válido. Valor esperado: ENTERO");
+            } while (!check);
+
+            return returnedInt;
+        }
+    }
+
 }
