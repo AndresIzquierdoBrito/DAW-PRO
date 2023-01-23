558d0301a12ddc91c9b01e74012ca43e7a2009ea
Andres Izquierdo <andres.izbri@gmail.com>
2023-01-23 01:59:52 +0000

    Completed all 3 recursive exercises from EJ0502

---
+++ Unidad 5 - Funciones/EJE0501 Ejercicios de Funciones y Recursividad/Recursivas/Program.cs
@@ -0,0 +1,22 @@
+﻿using Recursivas;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Ejercicio 6
+
+        //Functions.PrintNumbers(1);
+
+        //Ejercicio 7
+
+        //Console.WriteLine(Functions.IsPrime(2));
+
+        //Ejercicio 8
+
+        //string s = "abcabcabcd"; 
+        //char c = 'a';
+        //Console.WriteLine(Functions.letras(s, c));
+
+    }
+}
