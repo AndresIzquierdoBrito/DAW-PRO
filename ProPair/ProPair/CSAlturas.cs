9cc4b2fff1adcd86f7e62df8bb6a6214ad183154
Andres Izquierdo <andres.izbri@gmail.com>
2023-01-31 10:30:08 +0000

    Added Pair Programming exercise

---
+++ ProPair/ProPair/CSAlturas.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProPair
+{
+    internal class CSAlturas
+    {
+        public static string[] PersonasPorEncimaMedia(Personas[] listaPersonas)
+        {
+            int j = 0;
+            decimal avg = AlturaMedia(listaPersonas);
+            for (int i = 0; i < listaPersonas.Length; i++)
+            {
+                if (listaPersonas[i].altura > avg)
+                    j++;
+            }
+            string[] nombres = new string[j];
+            for (int i = 0; i < j; i++)
+            {
+                if (listaPersonas[i].altura > avg)
+                    nombres[i] = listaPersonas[i].nombre;
+            }
+            return nombres;
+        }
+        public static string[] PersonasPorDebajoMedia(Personas[] listaPersonas)
+        {
+            int j = 0;
+            for (int i = 0; i < listaPersonas.Length; i++)
+            {
+                if (listaPersonas[i].altura < AlturaMedia(listaPersonas))
+                    j++;
+            }
+            string[] nombres = new string[j];
+            for (int i = 0; i < j; i++)
+            {
+                if (listaPersonas[i].altura < AlturaMedia(listaPersonas))
+                    nombres[i] = listaPersonas[i].nombre;
+            }
+            return nombres;
+        }
+
+        public static void MostrarPersonas(string[] personasFiltradas)
+        {
+            Console.WriteLine("Lista de nombres: ");
+            for (int i = 0; i < personasFiltradas.Length; i++)
+            {
+                Console.WriteLine(personasFiltradas[i]);
+            }
+        }
+
+        public static decimal AlturaMedia(Personas[] listaPersonas)
+        {
+            decimal avgHeight = 0;
+            for (int i = 0; i < listaPersonas.Length; i++)
+            {
+                avgHeight += listaPersonas[i].altura;
+            }
+            return avgHeight/listaPersonas.Length;
+        }
+    }
+}
---
+++ ProPair/ProPair/Funciones.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProPair
+{
+    public struct Personas
+    {
+        public string nombre;
+        public string apellidos;
+        public decimal altura;
+    }
+    internal class CSFunciones
+    {
+        public static int SolicitarNumerosPersonas()
+        {
+            Console.WriteLine("Introduce la cantidad de personas a tratar: (ENTERO)");
+            return Validated.IntValue();
+        }
+        public static Personas[] LeerDatosMuestra(int personasMuestra)
+        {
+            Personas[] datosPersonas = new Personas[personasMuestra];
+            for (int i = 0; i < personasMuestra; i++)
+            {
+                Console.Write("\nNombre:");
+                datosPersonas[i].nombre = Validated.Name();
+                Console.Write("Apellidos:");
+                datosPersonas[i].apellidos = Console.ReadLine();
+                Console.Write("Altura (Valor positivo | Limite altura: 3M):");
+                datosPersonas[i].altura = Validated.DecimalValue(true, 3);
+            }
+
+            return datosPersonas;
+        }
+        public static void MostrarDatosMuestra(Personas[] listaPersonas)
+        {
+            for (int i = 0; i < listaPersonas.Length; i++)
+            {
+                Console.WriteLine($"Nombre de persona[{i}]: {listaPersonas[i].nombre}");
+                Console.WriteLine($"Apellido de persona[{i}]: {listaPersonas[i].apellidos}");
+                Console.WriteLine($"Altura de persona[{i}]: {listaPersonas[i].altura}\t");
+            }
+        }
+    }
+    internal class Validated
+    {
+        public static int IntValue()
+        {
+            bool check = false;
+            int returnedInt;
+            do
+            {
+                if (int.TryParse(Console.ReadLine(), out returnedInt))
+                    check = true;
+                else Console.WriteLine("Value not valid. Expected value: INTEGER");
+            } while (!check);
+
+            return returnedInt;
+        }
+        public static decimal DecimalValue(bool positive, int upperLimit) //Funcion reutilizable, pasandole en positive si quieres que sea positivo o negativo y en upperLimit el numero limite entero
+        {
+            decimal returnedDecimal;
+            bool decimalCheck, positiveCheck, limiteCheck;
+            do {
+                decimalCheck = false; positiveCheck = false; limiteCheck = false;
+                if (Decimal.TryParse(Console.ReadLine(), out returnedDecimal))
+                {
+                    decimalCheck = true;
+                    positiveCheck = (positive && returnedDecimal > 0) || !positive;
+
+                    if (upperLimit == 0)
+                        limiteCheck = true;
+                    else
+                        if (returnedDecimal < upperLimit)
+                            limiteCheck = true;
+                }
+            } while(!decimalCheck || !positiveCheck || !limiteCheck);
+            return returnedDecimal;
+        }
+        public static string Name()
+        {
+            string nameInput;
+            do
+            {
+                nameInput = Console.ReadLine();
+                if (nameInput.Trim().Length == 0)
+                    Console.WriteLine("Nombre no valido. No puede estar vacío");
+            } while (nameInput.Trim().Length == 0 );
+            return nameInput;
+        }
+    }
+}
