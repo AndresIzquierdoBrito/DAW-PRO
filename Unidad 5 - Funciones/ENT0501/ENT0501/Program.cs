e88ad8ba9174e972a88ad636a8a6db6631c21c7e
Andres Izquierdo <andres.izbri@gmail.com>
2023-02-03 02:01:42 +0000

    ENT0501 Completed

--- Unidad 5 - Funciones/ENT0501/ENT0501/Program.cs
+++ Unidad 5 - Funciones/ENT0501/ENT0501/Program.cs
@@ -1,7 +1,26 @@
-﻿internal class Program
+﻿
+using ENT0501;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] Casas; // Array donde se guardan nombres de las casas
+        double[] Costo; // Array donde se guardan el precio de las casas
+        int amountHouses;
+
+        Console.WriteLine("Input the amount of houses to store\n=================================");
+        amountHouses = Validated.IntValue(lowerLimit: 1, upperLimit: 30); //Limite de casas introducidas entre 10 y 30
+        Casas = new string[amountHouses];
+        Costo = new double[amountHouses];
+
+        Functions.RequestHouseData(ref Casas, ref Costo);
+        List<int> housesBetweenLimit = Functions.GetHousesBetweenPrice(ref Costo); // Guardamos en una lista los indices de cada casa entre un limite pedido al usuario
+        List<int> housesUnderPrice = Functions.GetHousesUnderPrice(ref Costo); // Guardamos en una lista los indices de cada casa bajo un precio pedido usuario
+
+        Console.Write("Houses between the range introduced\n=================================\n");
+        Functions.DisplayHousesList(housesBetweenLimit, ref Casas, ref Costo);
+        Console.Write("\nHouses under the price introduced\n=================================\n");
+        Functions.DisplayHousesList(housesUnderPrice, ref Casas, ref Costo);
     }
 }
