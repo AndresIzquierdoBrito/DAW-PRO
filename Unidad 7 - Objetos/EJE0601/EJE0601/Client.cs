14c48cf0a9e6127c0417c34133dc15bc05541f38
Andres Izquierdo <andres.izbri@gmail.com>
2023-03-24 13:33:36 +0000

    Foundation for EJE0601 - Objets exercises
    advanced in EJ01

---
+++ Unidad 7 - Objetos/EJE0601/EJE0601/Client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ01
+{
+    class Client
+    {
+        private string name;
+
+        private string firstSurname;
+
+        private string lastSurname;
+
+        private int id = 0;
+
+        public Client(string name, string firstSurname, string lastSurname)
+        {
+            this.name = name;
+            this.firstSurname = firstSurname;
+            this.lastSurname = lastSurname;
+
+            incrementId();
+            // id -> funcion
+        }
+
+        public int incrementId()
+        {
+            return id++; 
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string FirstSurname
+        {
+            get => firstSurname;
+            set => firstSurname = value;
+        }
+
+        public string LastSurname
+        {
+            get => lastSurname; 
+            set => lastSurname = value;
+        }
+
+        public int Id
+        {
+            get => id; 
+            set => id = value;
+        }
+    }
+}
---
+++ Unidad 7 - Objetos/EJE0601/EJE0601/Program.cs
@@ -0,0 +1,18 @@
+﻿using EJ01;
+using System.Diagnostics;
+
+internal class Program
+{
+        //Queremos informatizar una lista de clientes, de los que queremos saber el nombre y sus apellidos, que no deben ser nulos.A cada cliente, 
+        //le asignaremos un número que irá correlativo. Generaremos un menú que nos permita crear un cliente, buscar si un cliente pertenece a 
+        //la lista, mostrar los clientes de la lista y eliminar un cliente de la lista.
+
+    private static void Main(string[] args)
+    {
+        string name = "Pepe", surname = "Juanes", secondSurname = "Dolores";
+
+        Client c = new Client(name, surname, secondSurname);
+        Client c1 = new Client(name, surname, secondSurname);
+        Console.WriteLine(c1.Name + c1.FirstSurname + c1.LastSurname + c1.Id + c.Id);
+    }
+}
