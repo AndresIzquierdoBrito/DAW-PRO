acc64896fe961031960dcf6525ba308facbe69b1
Andres Izquierdo <andres.izbri@gmail.com>
2023-06-09 14:39:53 +0100

    Created some files

---
+++ Unidad 8 - ASP NET/ENT0701/Pages/Components/EjercicioBrandReport.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ENT0701.Pages.Components
+{
+    public class EjercicioBrandReportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
